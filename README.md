7dccdf12a70d1d1c28356a3221ce4ceae23979fd
gustavobarbosa-p <gustavobarbosa4383@gmail.com>
2022-10-29 13:20:55 -0300

    atualização das informações do segundo turno a cada 1 segundo de forma automatizada

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # eleicoes-2022
-Layout simples feito com a biblioteca tkinter de um sistema que mostra os resultados das eleições presidenciais em tempo real através dos dados do TSE (https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json)
+Layout simples feito com a biblioteca tkinter de um sistema que mostra os resultados das eleições presidenciais no 2° turno em tempo real através dos dados do TSE (https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json)
 
 Peguei o código originalmente em https://github.com/pycodebr/apuracao_eleicoes_2022 e fiz algumas modificações
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import requests
-import json
 from tkinter import *
+import json
+import requests
 
-def resultado_eleicoes():
+def buscar_resultados():
 
-    data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+    data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
     json_data = json.loads(data.content)
 
     candidato1 = []
@@ -16,10 +16,12 @@ def resultado_eleicoes():
             candidato1.append(informacoes['nm'])
             candidato1.append(informacoes['vap'])
             candidato1.append(informacoes['pvap'])
+            candidato1.append(informacoes['st'])
         if informacoes['seq'] in ['2']:
             candidato2.append(informacoes['nm'])
             candidato2.append(informacoes['vap'])
             candidato2.append(informacoes['pvap'])
+            candidato1.append(informacoes['st'])
 
     # pega as informações e coloca na string
     texto = f'''
@@ -30,17 +32,21 @@ def resultado_eleicoes():
     # edita o parâmetro "text" da variável "exibe"
     exibe['text'] = texto
 
-#------------------------JANELA------------------------#
+def run():
+    buscar_resultados()
+    window.after(1000, run)
+    window.update()
+
 window = Tk()
-window.title('Segundo Turno 2022')
+window.title('Apuração eleições 2022')
 
 txt_orientacao = Label(window, text="Clique no botão a para atualizar os resultados")
-txt_orientacao.grid(column = 0, row = 0, padx = 10, pady = 10)
+txt_orientacao.grid(column=0, row=0, padx=10, pady=10)
 
-botao = Button(window, text = "Atualizar Resultados", command = resultado_eleicoes)
-botao.grid(column = 0, row = 1, padx = 10, pady = 10)
+botao = Button(window, text = "Atualizar Resultados", font=('Arial', 10), command=run)
+botao.grid(column=0, row=1, padx=10, pady=10)
 
-exibe = Label(window, text = 'indisponível')
-exibe.grid(column = 0, row = 2, padx = 20, pady = 20)
+exibe = Label(window, text='indisponível', bg='#f2f2f2')
+exibe.grid(column=0, row=2, padx=10, pady=20)
 
 window.mainloop()
