f38dca7a81c53b25222af81346d63eb3873dbc5a
gustavobarbosa-p <gustavobarbosa4383@gmail.com>
2022-10-30 01:43:52 -0300

    Atualização

--- main.py
+++ main.py
@@ -7,30 +7,39 @@ def buscar_resultados():
     data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
     json_data = json.loads(data.content)
 
-    candidato1 = []
-    candidato2 = []
-
-    # Busca e anexa as informações dos candidatos
-    for informacoes in json_data['cand']:
+    candidato1 = ['']
+    candidato2 = ['']
+    secoesApuradas = []
+    
+    for informacoes in json_data['cand']: # Busca e anexa as informações dos candidatos
         if informacoes['seq'] in ['1']:
+            candidato1[0] = (informacoes['st'])
             candidato1.append(informacoes['nm'])
             candidato1.append(informacoes['vap'])
             candidato1.append(informacoes['pvap'])
-            candidato1.append(informacoes['st'])
         if informacoes['seq'] in ['2']:
+            candidato2[0] = (informacoes['st'])
             candidato2.append(informacoes['nm'])
             candidato2.append(informacoes['vap'])
             candidato2.append(informacoes['pvap'])
-            candidato1.append(informacoes['st'])
+    
+    # Busca e anexa a porcentagem de seções apuradas
+    secoesApuradas.append(json_data['psa'])
+    pctSecoesApuradas['text'] = f'{secoesApuradas[0]}%'
 
     # pega as informações e coloca na string
-    texto = f'''
-    {candidato1[0]}: {candidato1[2]+'% '} {'|  ' + candidato1[1] + ' votos'}
-    -----------------------------------------------------------------------
-    {candidato2[0]}: {candidato2[2]+'% '} {'|  ' + candidato2[1] + ' votos'}'''
+    texto1 = f'''
+    {'1° - ' + candidato1[1]}: {candidato1[3]+'% '} {'|  ' + candidato1[2] + ' votos'}'''
+    exibeCandidato1['text'] = texto1
 
-    # edita o parâmetro "text" da variável "exibe"
-    exibe['text'] = texto
+    texto2 = f'''
+    {'2° - ' + candidato2[1]}: {candidato2[3]+'% '} {'|  ' + candidato2[2] + ' votos'}'''
+    exibeCandidato2['text'] = texto2
+
+    if candidato1[0] == 'Eleito':
+        exibeCandidato1['bg'] == '#1aff1a'
+    if candidato2[0] == 'Eleito':
+        exibeCandidato2['bg'] == '#1aff1a'
 
 def run():
     buscar_resultados()
@@ -43,10 +52,16 @@ window.title('Apuração eleições 2022')
 txt_orientacao = Label(window, text="Clique no botão a para atualizar os resultados")
 txt_orientacao.grid(column=0, row=0, padx=10, pady=10)
 
-botao = Button(window, text = "Atualizar Resultados", font=('Arial', 10), command=run)
+botao = Button(window, text = "Atualizar Resultados", font=('Arial', 8), command=run)
 botao.grid(column=0, row=1, padx=10, pady=10)
 
-exibe = Label(window, text='indisponível', bg='#f2f2f2')
-exibe.grid(column=0, row=2, padx=10, pady=20)
+exibeCandidato1 = Label(window, text='indisponível', bg='#f2f2f2')
+exibeCandidato1.grid(column=0, row=2, padx=10)
+
+exibeCandidato2 = Label(window, text='indisponível', bg='#f2f2f2')
+exibeCandidato2.grid(column=0, row=3, padx=10)
+
+pctSecoesApuradas = Label(window, text='', bg='#f2f2f2')
+pctSecoesApuradas.grid(column=1, row=1, padx=10, pady=20)
 
 window.mainloop()
